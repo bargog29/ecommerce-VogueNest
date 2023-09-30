72c778e274545d2759e871776cbc43ff8059a5df
bargog29 <baranga.georgemarius@gmail.com>
2023-09-30 21:53:53 +0300

    .gitignore is now working

--- dist/index.8b0ecb76.js
+++
@@ -1,3 +0,0 @@
-const e=document.getElementById("bar"),t=document.getElementById("navbar"),c=document.getElementById("close");e&&e.addEventListener("click",()=>{t.classList.add("active")}),c&&c.addEventListener("click",()=>{t.classList.remove("active")});//# sourceMappingURL=index.8b0ecb76.js.map
-
-//# sourceMappingURL=index.8b0ecb76.js.map
