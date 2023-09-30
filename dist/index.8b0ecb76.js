d1d02bc72489124c19908de6cb16d84c069209fe
bargog29 <baranga.georgemarius@gmail.com>
2023-09-30 21:28:39 +0300

    First Commit: Homepage

---
+++ dist/index.8b0ecb76.js
@@ -0,0 +1,3 @@
+const e=document.getElementById("bar"),t=document.getElementById("navbar"),c=document.getElementById("close");e&&e.addEventListener("click",()=>{t.classList.add("active")}),c&&c.addEventListener("click",()=>{t.classList.remove("active")});//# sourceMappingURL=index.8b0ecb76.js.map
+
+//# sourceMappingURL=index.8b0ecb76.js.map
